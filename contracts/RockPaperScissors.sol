a6cf698d88adbda384b91521fa0c7a1e2c8b82a7
n_44o <nfurfaro33@gmail.com>
2017-11-16 11:25:05 -0700

    Add test for proof of success. Currently 1 assertion error

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.6;
 
 
 contract RockPaperScissors {
+    bool public frozen;
     address public owner;
-    address public player;
     address public Alice;
     address public Bob;
     uint public ante;
     uint public playersReady;
-    uint public gameStatus;
+    // uint public gameStatus;
 
 
     struct PlayerData {
@@ -18,23 +18,18 @@ contract RockPaperScissors {
 
     mapping(address => PlayerData) contestants; 
     
-    event LogPlay(address player, uint deposit,  string codes, uint play);
-    event LogGameState(uint _playersReady, uint balance, string codes, uint _gameStatus);
-    event LogWinnings(string alices, uint amount1, string bobs, uint amount2);
+    event LogPlay(address player, uint deposit, uint play);
+    event LogGameOver(uint gameOutcome, uint AlicesWinnings, uint BobsWinnings);
     event LogWithdrawl(address withdrawer, uint amount);
+    event LogFreeze(bool isFrozen);
 
-    function RockPaperScissors()
+    function RockPaperScissors(address _Alice, address _Bob, uint _anteAmount)
         public
     {
         owner = msg.sender;
-        //remix
-        // Alice = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
-        // Bob = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
-        //testrpc
-        Alice = 0xa653157c9f2b34263f456953fd85de0211ce40e3;
-        Bob = 0xd996973097a0290ea7b025a5ad48863884c3bc30;
-        // ante = _anteAmount;
-        ante = 10;
+        ante = _anteAmount;
+        Alice = _Alice;
+        Bob = _Bob;
     }
 
     modifier isAliceOrBob() {
@@ -42,45 +37,50 @@ contract RockPaperScissors {
         _;
     }
 
-    modifier meetsRequirements() {
-        require(msg.value == ante);
-        require(contestants[msg.sender].hand == 0);
+    modifier freezeRay() {
+        require(!frozen);
         _;
     }
 
     function play(uint _hand)
-        isAliceOrBob
-        meetsRequirements 
+        freezeRay
+        isAliceOrBob 
         public 
         payable 
         returns (bool hasPlayed)
     {
+        require(msg.value == ante);
+        require(contestants[msg.sender].hand == 0);
         require(_hand != 0);
+        require(_hand < 4);
+        require(playersReady <= 1);
+        uint gameStatus;
+        uint stakes;
+        stakes += msg.value;
         if(playersReady == 0) {
             playersReady++;
             contestants[msg.sender].hand = _hand;
-            LogPlay(msg.sender, msg.value, "(1 = rock, 2 = paper, 3 = scissors)", _hand);
-            LogGameState(playersReady, this.balance, "(For Alice:  0 = undecided, 1 = Lose , 2 = draw, 3 = win)",  gameStatus);
+            LogPlay(msg.sender, msg.value, _hand);
             return true;
         } else if(playersReady == 1) {
             playersReady++;
             contestants[msg.sender].hand = _hand;
-            LogPlay(msg.sender, msg.value, "(1 = rock, 2 = paper, 3 = scissors)", _hand);
-            referee();
-            LogGameState(playersReady, this.balance, "(For Alice:  0 = undecided, 1 = Lose , 2 = draw, 3 = win)",  gameStatus);
+            LogPlay(msg.sender, msg.value, _hand);
+            gameStatus = referee();
+            require(gameStatus != 0);
             playersReady = 0;
             contestants[Alice].hand = 0;
             contestants[Bob].hand = 0;
             if(gameStatus == 2) {
+                contestants[Alice].winnings += stakes / 2;
+                contestants[Bob].winnings += stakes / 2;
+                LogGameOver(gameStatus, contestants[Alice].winnings, contestants[Bob].winnings);
                 gameStatus = 0;
-                contestants[Alice].winnings += ante;
-                contestants[Bob].winnings += ante;
-                LogWinnings("Alice's winnings:", contestants[Alice].winnings, "Bob's winnings:", contestants[Bob].winnings);
                 return true;
             } else {
+                contestants[msg.sender].winnings += stakes * 2;
+                LogGameOver(gameStatus, contestants[Alice].winnings, contestants[Bob].winnings);
                 gameStatus = 0;
-                contestants[msg.sender].winnings += ante * 2;
-                LogWinnings("Alice's winnings:", contestants[Alice].winnings, "Bob's winnings:", contestants[Bob].winnings);
                 return true;
             }
         }
@@ -88,40 +88,50 @@ contract RockPaperScissors {
     } 
     
     function withdraw()
-        isAliceOrBob
+        freezeRay
         public
         returns (bool success)
     {
         require(contestants[msg.sender].winnings != 0);
         uint amount = contestants[msg.sender].winnings;
-        LogWithdrawl(msg.sender, amount);
+        contestants[msg.sender].winnings = 0;
         msg.sender.transfer(amount);
+        LogWithdrawl(msg.sender, amount);
         return true;
     }
    
-    function referee() 
-        internal 
+   // For Alice:  0 = undecided, 1 = Lose , 2 = draw, 3 = win
+   // 1 = rock, 2 = paper, 3 = scissors
+    function referee()
+        public
+        constant
+        returns (uint returnValue) 
     {
         require(playersReady == 2);
         uint A = contestants[Alice].hand;
         uint B = contestants[Bob].hand;
         if(A == 1) {
-            if(B == 1) gameStatus = 2;
-            if(B == 2) gameStatus = 1;
-            if(B == 3) gameStatus = 3;
+            if(B == 1) return 2;
+            if(B == 2) return 1;
+            if(B == 3) return 3;
         } else if(A == 2) {
-            if(B == 2) gameStatus = 2;
-            if(B == 3) gameStatus = 1;
-            if(B == 1) gameStatus = 3;
+            if(B == 2) return 2;
+            if(B == 3) return 1;
+            if(B == 1) return 3;
         } else if(A == 3) {
-            if(B == 3) gameStatus = 2;
-            if(B == 1) gameStatus = 1;
-            if(B == 2) gameStatus = 3;
+            if(B == 3) return 2;
+            if(B == 1) return 1;
+            if(B == 2) return 3;
+        } else {
+            return 0;
         }
     }
 
-    function getLogs() public {
-        LogGameState(playersReady, this.balance, "(For Alice:  0 = undecided, 1 = Lose , 2 = draw, 3 = win)",  gameStatus);
-        LogWinnings("Alice's winnings:", contestants[Alice].winnings, "Bob's winnings:", contestants[Bob].winnings);
+    function freezerSwitch(bool _freeze)
+        returns (bool success) {
+            require(msg.sender == owner);
+            frozen = _freeze;
+            LogFreeze(frozen);
+            return true;
     }
 }
--- test/RockPaperScissors.js
+++ test/RockPaperScissors.js
@@ -1,41 +1,78 @@
 const RockPaperScissors = artifacts.require("./RockPaperScissors.sol");
-const chai = require('chai');
-const assert = chai.assert;
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-const Promise = require("bluebird");
 
 
 contract('RockPaperScissors', accounts => {
 	let owner = accounts[0];
 	let Alice = accounts[1];
 	let Bob = accounts[2];
+    let ante = 8000000000000000;
 
     beforeEach(() => {
-        return RockPaperScissors.new({ from: owner }).then(_instance => {
+        return RockPaperScissors.new(Alice, Bob, ante, { from: owner }).then(_instance => {
             instance = _instance;	
         })
     });
 
     it("should be owned by owner", () => {
-  	    return assert.eventually.strictEqual(Promise.resolve(instance.owner({from: owner})), owner, "Contract is not owned by owner");
+        return instance.owner({from: owner}).then(_owner => {
+            assert.strictEqual(_owner, owner, "Contract is not owned by owner");
+        }) 
     });
 
     it("should let Alice or Bob play", () => {
-        instance.play(1, {from: Alice, value: 10}).then((txObj) => {
-            console.log(txObj.logs[1].args);
-        return assert.eventually.equal(Promise.resolve(txObj.logs[0].args.play.toString(10)), "1", "Player's hand wasn't registered or interpreted correctly");
+        return instance.play(1, {from: Alice, value: 8000000000000000}).then((txObj) => {
+            assert.equal(txObj.logs[0].args.play.toString(10), "1", "Player's hand wasn't registered or interpreted correctly");
         })
     });
 
     it("should decide the match fairly", () => {
-        instance.play(1, {from: Alice, value: 10}).then(() => {
-            return instance.play(3, {from: Bob, value: 10})
+        return instance.play(1, {from: Alice, value: 8000000000000000}).then(() => {
+            return instance.play(3, {from: Bob, value: 8000000000000000})
         }).then((txObj) => {
-            console.log(txObj)
-            // assert.equal(txObj.logs[0].args.play.toString(10), "1", "Player's hand wasn't registered or interpreted correctly");
+            assert.equal(txObj.logs[1].args.gameOutcome.toString(10), "3", "referee() is not being fair");
+            assert.equal(txObj.logs[1].args.AlicesWinnings.toString(10), "0", "error with contract accounting");
+            assert.equal(txObj.logs[1].args.BobsWinnings.toString(10), "16000000000000000", "error with contract accounting");
         })    
     })
 
-    
+    it("should allow a player to withdraw their winnings", () => {
+        let startBalance;
+        let winnings;
+        let gasPrice;
+        let gasUsed;
+        let txFee;
+        let endBalance;
+        startBalance = web3.eth.getBalance(Bob);
+        console.log("startBalance: " + startBalance.toString(10));
+        return instance.play(1, {from: Alice, value: 8000000000000000}
+        ).then(() => {
+            return instance.play(3, {from: Bob, value: 8000000000000000})
+        }).then(() => {
+            return instance.withdraw({from: Bob})          
+        }).then(txObj => {
+            gasUsed = txObj.receipt.gasUsed;
+            winnings = txObj.logs[0].args.amount
+            console.log("Winnings: " + winnings.toString(10));
+            return web3.eth.getTransaction(txObj.tx, (err, tx) => {
+                gasPrice = tx.gasPrice;
+                txFee = gasPrice.times(gasUsed);
+                endBalance = web3.eth.getBalance(
+                Bob);
+                console.log("gasUsed: " + gasUsed);
+                console.log("gasPrice: " + gasPrice);
+                console.log("txFee: " + txFee.toString(10)); 
+                console.log("endBalance: " + endBalance.toString(10));
+                console.log("delta: " + endBalance.minus(startBalance).toString(10));
+                assert.equal(startBalance.plus(winnings).minus(txFee).toString(10), endBalance.toString(10), "Bob didn't get his ether")
+            })       
+        })
+
+    })
 })
+
+
+
+
+// web3.eth.getTransactionReceipt(transactionHash, function(err, transaction) {
+//     console.info(transaction);    
+//   })
